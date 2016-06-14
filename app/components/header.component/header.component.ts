1b948359fb586763abcaa00b4ad9f6e955d1fd08
danielalvenstrand <daniel.alvenstrand@capgemini.com>
2016-06-14 17:04:02 +0200

    Fixed structure

---
+++ app/components/header.component/header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'header-component',
+  templateUrl: 'app/components/header.component/header.component.html'
+})
+export class HeaderComponent { }
+
