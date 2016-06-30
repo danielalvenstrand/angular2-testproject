a8dc49e3c56fe55f075210ae03627e88a005c3cf
danielalvenstrand <daniel.alvenstrand@capgemini.com>
2016-06-30 16:40:37 +0200

    removed app dir

--- app/example/example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'example-component',
-  templateUrl: 'app/example/example.component.html',
-  styleUrls: ['app/example/example.component.css']
-})
-export class ExampleComponent { }
-
--- app/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppService } from './app.service';
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent,[AppService]);
