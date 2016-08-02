8a38d79a0599fefb3f43c685bae2dbb1d8b5e871
danielalvenstrand <daniel.alvenstrand@capgemini.com>
2016-08-02 14:01:02 +0200

    Disabled ghostmode

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 
+//import { enableProdMode } from '@angular/core'
+//enableProdMode();
+
 bootstrap(AppComponent,[
     disableDeprecatedForms(), provideForms(), Title,
     HTTP_PROVIDERS, APP_ROUTER_PROVIDERS
