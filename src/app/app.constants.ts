3ee4e038744ddbec3209e41ab05606868106b80e
danielalvenstrand <daniel.alvenstrand@capgemini.com>
2016-08-02 11:03:03 +0200

    Full connection to API online

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server: string = "http://localhost:8000/";
+    public Server: string = "http://ec2-52-208-98-0.eu-west-1.compute.amazonaws.com:8000/";
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
